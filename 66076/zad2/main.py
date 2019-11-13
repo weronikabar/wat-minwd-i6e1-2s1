be194dccb997e8bf4c0cd12bbcc976b748d56e42
Piotr Filochowski <piotr.filochowski01@gmail.com>
2019-11-13 20:07:55 +0100

    saving file on disc to ./output/ dir

--- 66076/zad2/main.py
+++ 66076/zad2/main.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 import bs4 as soup
 import io
+import sys
+import os
 
 def getInput():
     # TODO: odczytac input z pliku
@@ -11,9 +13,22 @@ def getInput():
 def scrap(url):
     response = urlopen(url)
     bsObj = bsObj = soup.BeautifulSoup(response.read(), 'lxml')
-    with io.open("filename", 'w', encoding='utf-8') as openedFile:
+    with io.open(createFileName(bsObj.head.title.getText()), 'w+', encoding='utf-8') as openedFile:
         for tableRow in bsObj.find_all('tr'): 
             openedFile.write(tableRow.get_text())
+
+
+def createFileName(string):
+    # File name can't contain: \ / * ? < > | 
+    # Let's convert those chars for _ 
+    # File name can't be longer that 260 on windows (i think) but lets cut it to 50 chars to make it more beautiful
+    outputFileName = string.replace("\\", "_").replace("/", "_").replace("*", "_").replace("?", "_").replace("<", "_").replace(">", "_").replace("|", "_").replace(":", "_").replace("&", "_")
+    outputFileName = outputFileName[:50]
+
+    current_work_directory = os.getcwd()
+    abs_work_directory = os.path.abspath(current_work_directory)
+
+    return "./output/" + outputFileName
 
 
 def main():
