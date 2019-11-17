a4956019233506fc262a29db4ca6ae00632115ac
Piotr Filochowski <piotr.filochowski@novaris.pl>
2019-11-17 10:24:15 +0100

    Photo download + sample page

--- 66076/zad2/main.py
+++ 66076/zad2/main.py
@@ -1,21 +1,25 @@
 import io
 import os
+from urllib.error import HTTPError
 from urllib.request import urlopen
 
 import bs4 as soup
 import jsons
+import wget
 
 
 def main():
-    input = getInput()
+    input = get_input()
     for url in input:
         scrap(url)
 
-def getInput():
+
+def get_input():
     # TODO: odczytac input z pliku
     input = []
     input.append("https://pl.wiktionary.org/wiki/Indeks:Angielski_-_Jedzenie")
     return input
+
 
 def scrap(url):
     response = urlopen(url)
@@ -23,41 +27,62 @@ def scrap(url):
     model = Model()
     model.language1 = "polski"
     model.language2 = "angielski"
-    with io.open(createFileName(bsObj.head.title.getText()), 'w+', encoding='utf-8') as openedFile:
+    file_name = create_file_name(bsObj.head.title.getText())
+    with io.open(file_name, 'w+', encoding='utf-8') as openedFile:
         for tableRow in bsObj.find_all('tr'):
-            word = getWord(tableRow)
+            word = get_word_and_save_photo(tableRow, file_name)
             model.words.append(word)
         openedFile.write(jsons.dumps(model))
 
-def createFileName(string):
+
+def create_file_name(string):
     # File name can't contain: \ / * ? < > | & 
     # Let's convert those chars for _ 
     # File name can't be longer that 260 on windows (i think) but lets cut it to 50 chars to make it more beautiful
-    outputFileName = string.replace("\\", "_").replace("/", "_").replace("*", "_").replace("?", "_").replace("<", "_").replace(">", "_").replace("|", "_").replace(":", "_").replace("&", "_")
+    outputFileName = string.replace("\\", "_").replace("/", "_").replace("*", "_").replace("?", "_").replace("<",
+                                                                                                             "_").replace(
+        ">", "_").replace("|", "_").replace(":", "_").replace("&", "_")
     outputFileName = outputFileName[:50]
     current_work_directory = os.getcwd()
     return "./output/" + outputFileName + ".json"
 
-def getWord(tableRow):
+
+def save_photo(photo_address, filename):
+    address = photo_address.split()
+    url = 'https:' + address[0]
+    # image = urllib.URLopener()
+    # image.retrieve(url)
+    # local_file = open('local_image.jpg', 'wb')
+    # shutil.copyfileobj(image, local_file)
+    try:
+        local_image_filename = wget.download(url, './output/')
+        return '/output/' + local_image_filename
+    except (IndexError, HTTPError):
+        print('error occured')
+        return '?'
+
+
+def get_word_and_save_photo(table_row, filename):
     word = Word()
-    cells = tableRow.find_all('td')
-    if (len(cells) == 0):
+    cells = table_row.find_all('td')
+    if len(cells) == 0:
         return Word()
-    if (len(cells) == 3):  # with photo
+    if len(cells) == 3:  # with photo
         word.language1 = cells[0].getText()
         word.language2 = cells[2].getText()
         img = cells[1].find_all('img')
-        if (len(img) > 0):
-            href = img[0]['srcset']
-            word.photo = href
+        if len(img) > 0:
+            photo_adress = img[0]['srcset']
+            word.photo = save_photo(photo_adress, filename)
         else:
-            word.photo = 'dupa'
+            word.photo = 'null'
         return word
-    if (len(cells) == 2):
+    if len(cells) == 2:
         word.language1 = cells[0].getText()
         word.language2 = cells[1].getText()
         word.photo = "null"
         return word
+
 
 class Model:
     language1 = ""
@@ -72,4 +97,3 @@ class Word:
 
 
 main()
-
