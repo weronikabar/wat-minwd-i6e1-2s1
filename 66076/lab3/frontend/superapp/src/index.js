d063abcbf7f4b3f204958461e8d2ce894ea47e23
Piotr Filochowski <piotr.filochowski@novaris.pl>
2019-11-20 10:37:23 +0100

    Sample GoJs code for beggining

--- 66076/lab3/frontend/superapp/src/index.js
+++ 66076/lab3/frontend/superapp/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as go from 'gojs';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+
+const gojsKey = process.env.REACT_APP_GOJS_KEY;
+
+if (gojsKey) {
+    // tslint:disable-next-line:no-any
+    go.licenseKey = gojsKey;
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
