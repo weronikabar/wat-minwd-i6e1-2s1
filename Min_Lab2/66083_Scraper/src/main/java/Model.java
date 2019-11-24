83cad27e9ad8e9a2f8ff16715b9d34bb52d5494f
PGiermakowska <41075936+PGiermakowska@users.noreply.github.com>
2019-11-24 22:28:03 +0100

    Added lab_2

---
+++ Min_Lab2/66083_Scraper/src/main/java/Model.java
@@ -0,0 +1,30 @@
+public class Model {
+    private String language1;
+    private String language2;
+    private String url;
+
+
+    public String getLanguage1() {
+        return language1;
+    }
+
+    public void setLanguage1(String language1) {
+        this.language1 = language1;
+    }
+
+    public String getLanguage2() {
+        return language2;
+    }
+
+    public void setLanguage2(String language2) {
+        this.language2 = language2;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
